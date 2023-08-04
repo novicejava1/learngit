a80decfc806960151b8a235b5e15e605e3ac203e
novicejava1 <sudhirbhoga@gmail.com>
2023-08-04 07:52:37 +0530

    update hello-runner formatting

--- hello-runner.sh
+++ hello-runner.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "This script captures the custom env variables and prints their value"
-
+echo "====================================================================="
 echo "language: '$1'"
 echo "message: '$2'"
+echo "====================================================================="
+
 
